8f7530ce7be7ab7d96e273723da68c5c486fbdb5
Artem Shush <shushtain@gmail.com>
2025-07-05 14:01:08 +0300

    style: .stylua.toml

--- lua/farba/plugins/which-key.lua
+++ lua/farba/plugins/which-key.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 function M.setup(palette)
-	local background = palette.background and palette.general.gray.v10 or "NONE"
-	local groups = {
-		WhichKeyNormal = { fg = palette.general.gray.v80, bg = background },
-	}
+  local background = palette.background and palette.general.gray.v10 or "NONE"
+  local groups = {
+    WhichKeyNormal = { fg = palette.general.gray.v80, bg = background },
+  }
 
-	for key, value in pairs(groups) do
-		vim.api.nvim_set_hl(0, key, value)
-	end
+  for key, value in pairs(groups) do
+    vim.api.nvim_set_hl(0, key, value)
+  end
 end
 
 return M
